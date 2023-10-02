b642cbc923e35d5c0eba43aec0439bbce7de35f0
SureshKuchana <kuchana123.suresh@gmail.com>
2023-10-02 21:01:41 +0530

    removed tailwind, configured with chakra UI & set up with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className='text text:500'>Game Hub</div>;
+  return (
+    <Grid
+      templateAreas={{
+        // eslint-disable-next-line quotes
+        base: `"nav" "main"`,
+        // eslint-disable-next-line quotes
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav'>
+        <NavBar />
+      </GridItem>
+      <Show above='lg'>
+        <GridItem area='aside'>Aside</GridItem>
+      </Show>
+      <Show above='lg'>
+        <GridItem area='main'>Main</GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
